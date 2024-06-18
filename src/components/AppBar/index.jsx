758dd018ca5302a2ca34fb17025d92dab71aa9e2
ErykCode <phamminhduc5753@gmail.com>
2024-06-19 02:10:43 +0700

    AppBar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,9 +1,14 @@
 import ModeSelect from '~/components/ModeSelect'
 import Box from '@mui/material/Box'
-import AppsIcon from '@mui/icons-material/Apps';
-import SvgIcon from '@mui/material/SvgIcon';
+import AppsIcon from '@mui/icons-material/Apps'
+import SvgIcon from '@mui/material/SvgIcon'
 import { ReactComponent as trelloIcon } from '~/assets/trello.svg'
-import Typography from '@mui/material/Typography';
+import Typography from '@mui/material/Typography'
+import WorkSpaces from './DropMenus/WorkSpaces'
+import Starred from './DropMenus/Starred'
+import Recent from './DropMenus/Recent'
+import Templates from './DropMenus/Templates'
+import Button from '@mui/material/Button'
 
 function AppBar() {
   return (
@@ -13,11 +18,19 @@ function AppBar() {
         justifyContent: 'space-between'
       }}>
 
-        <Box>
-        <AppsIcon sx={{color: 'primary.main'}}></AppsIcon>
-        <SvgIcon component={trelloIcon} inheritViewBox sx={{color: 'primary.main'}} />
-        <Typography variant="span">Trello</Typography>
-        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2}}>
+          <AppsIcon  sx={{color: 'primary.main'}}></AppsIcon>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5}}>
+            <SvgIcon component={trelloIcon} inheritViewBox sx={{color: 'primary.main'}} />
+            <Typography variant="span" sx={{fontWeight: 'bold', fontSize: '1.5rem', color: 'primary.main'}}>Trello</Typography>
+            <WorkSpaces></WorkSpaces>
+            <Recent></Recent>
+            <Starred></Starred>
+            <Templates></Templates>
+
+            <Button variant="outlined">Create</Button>
+          </Box>
+        </Box>  
 
         <Box>
         <ModeSelect />
