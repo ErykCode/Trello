774dea0c06ca99ab20d9fa8ef55b8cc5782b692b
ErykCode <phamminhduc5753@gmail.com>
2024-06-20 02:49:27 +0700

    laymelaytroi

--- src/Pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/Pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -1,7 +1,5 @@
 import Column from './Column/Column'
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
 
 
 function ListColumns() {
@@ -19,24 +17,7 @@ function ListColumns() {
       <Column />
       <Column />
 
-      <Box
-        sx={{
-          minWidth: '200px',
-          maxWidth: '200px',
-          mx: 2, borderRadius: '5px',
-          height: 'fit-content', bgcolor: '#ffffff3d'
-        }}
-      >
-        <Button
-          startIcon={<NoteAddIcon />}
-          sx={{
-            color: 'white', with: '100%', justifyContent: 'flex-start',
-            pl: 2.5, py: 1
-          }}
-        >
-          Add New Colum
-        </Button>
-      </Box>
+      
     </Box>
   )
 }
